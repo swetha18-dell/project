72d3ecf9becef47e22e1b90931bc5ead6b626d51
swetha18-dell <113038453+swetha18-dell@users.noreply.github.com>
2024-06-11 13:16:04 +0530

    Create PlacePins.tcl

---
+++ rtl2signoff/pd/scripts/PlacePins.tcl
@@ -0,0 +1,10 @@
+et_pin_physical_constraints -pin_name VDD -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 11.39
+set_pin_physical_constraints -pin_name VSS -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 12.7
+
+set_pin_physical_constraints -pin_name Clk -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 20
+set_pin_physical_constraints -pin_name reset -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 23
+set_pin_physical_constraints -pin_name UpOrDown -layers {M2 M3} -width 0.28 -depth 0.28 -side 1 -offset 26
+set_pin_physical_constraints -pin_name Count[0] -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 70
+set_pin_physical_constraints -pin_name Count[1] -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 73
+set_pin_physical_constraints -pin_name Count[2] -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 76
+set_pin_physical_constraints -pin_name Count[3] -layers {M2 M3}  -width 0.28 -depth 0.28 -side 1 -offset 79
